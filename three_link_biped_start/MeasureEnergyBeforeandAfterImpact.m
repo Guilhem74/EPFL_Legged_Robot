f7bceb1915f660cbd02d3a2e85f270f933bd0982
Guilhem AZZANO <g.azzano74@gmail.com>
2018-10-30 20:17:30 +0100

    End of session 2
    Remove temp files for the evals
    Added Hedi's code for the impact
    Seems to work well.

---
+++ three_link_biped_start/MeasureEnergyBeforeandAfterImpact.m
@@ -0,0 +1,5 @@
+q_m = [pi/6, -pi/6, pi/10];
+dq_m = [1, 0.2, 0];
+[T_m, V_m] = eval_energy(q_m, dq_m)
+[q_p, dq_p] = impact(q_m, dq_m);
+[T_p, V_p] = eval_energy(q_p, dq_p)
--- three_link_biped_start/dynamics/impact.m
+++ three_link_biped_start/dynamics/impact.m
@@ -4,15 +4,16 @@
 % 
 function [q_p, dq_p] = impact(q_m, dq_m)
 
-% q_p(1, 1) = 
-% q_p(2, 1) = 
-% q_p(3, 1) = 
+q_p(1) = q_m(2);
+q_p(2) = q_m(1);
+q_p(3) = q_m(3);
 
 A_m = eval_A_m(q_m);
 A_p = eval_A_p(q_p);
 
 % A_p dq_p = A_m dq_m
 % Note: To solve the equation Ax = b you could use x = A \ b to avoid taking
-% inverse of A. 
-
+% inverse of A.
+C=A_m*dq_m';
+dq_p=(inv(A_p)*(C))'; 
 end
