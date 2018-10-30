6c2b39fa7c6d05c6ef3e13c5597c778da1b87226
Guilhem AZZANO <g.azzano74@gmail.com>
2018-10-30 21:12:00 +0100

    refresh some file + add questions

--- three_link_biped_start/dynamics/impact.m
+++ three_link_biped_start/dynamics/impact.m
@@ -8,7 +8,7 @@ q_p(1) = q_m(2);
 q_p(2) = q_m(1);
 q_p(3) = q_m(3);
 
-dq_m=[dq_m(1) dq_m(2) dq_m(3)]; % Accept Column or row vector and will be able to use it either case.
+dq_m=[dq_m(1) dq_m(2) dq_m(3)]'; % Accept Column or row vector and will be able to use it either case.
 
 A_m = eval_A_m(q_m);
 A_p = eval_A_p(q_p);
@@ -16,6 +16,7 @@ A_p = eval_A_p(q_p);
 % A_p dq_p = A_m dq_m
 % Note: To solve the equation Ax = b you could use x = A \ b to avoid taking
 % inverse of A.
-C=A_m*dq_m';
-dq_p=(inv(A_p)*(C))'; 
+%C=(A_m*dq_m)
+%dq_p=(inv(A_p)*(C))'; 
+dq_p=A_p\(A_m*dq_m);
 end
