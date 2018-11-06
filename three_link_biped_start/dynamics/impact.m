b5c99659ec02866d1a222949fe1b700c0b08042f
Guilhem AZZANO <g.azzano74@gmail.com>
2018-11-06 12:59:59 +0100

    Assignement3files

--- three_link_biped_start/dynamics/impact.m
+++ three_link_biped_start/dynamics/impact.m
@@ -4,19 +4,14 @@
 % 
 function [q_p, dq_p] = impact(q_m, dq_m)
 
-q_p(1) = q_m(2);
-q_p(2) = q_m(1);
-q_p(3) = q_m(3);
-
-dq_m=[dq_m(1) dq_m(2) dq_m(3)]'; % Accept Column or row vector and will be able to use it either case.
+q_p(1, 1) = q_m(2);
+q_p(2, 1) = q_m(1);
+q_p(3, 1) = q_m(3);
 
 A_m = eval_A_m(q_m);
 A_p = eval_A_p(q_p);
 
 % A_p dq_p = A_m dq_m
-% Note: To solve the equation Ax = b you could use x = A \ b to avoid taking
-% inverse of A.
-%C=(A_m*dq_m)
-%dq_p=(inv(A_p)*(C))'; 
-dq_p=A_p\(A_m*dq_m);
+dq_p = A_p \ (A_m * dq_m);
+
 end
