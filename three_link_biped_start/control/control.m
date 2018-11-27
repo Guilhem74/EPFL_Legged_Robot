1d60ac28f55299496f131024e91aba696c8cbe58
Guilhem AZZANO <g.azzano74@gmail.com>
2018-11-27 13:12:19 +0100

    Added control of u1
    Torso controlled

--- three_link_biped_start/control/control.m
+++ three_link_biped_start/control/control.m
@@ -1,7 +1,20 @@
-function u = control(t, q, dq, q0, dq0, step_number)
-% You may call control_hyper_parameters and desired_outputs in this
-% function
-% you don't necessarily need to use all the inputs to this control function
-
+function u = control(q, dq)
+u = zeros(2, 1);
+Kp=500;
+Kd=280;
+q1=q(1);
+q2=q(2);
+q3=q(3)
+dq1=dq(1);
+dq2=dq(2);
+dq3=dq(3);
+u1=Kp*(q3-pi/2)+Kd*dq3;
+if(u1>30)
+    u1=30;
+elseif(u1<-30)
+    u1=-30;
+end;
+u(1)=u1;
+ 
 
 end
