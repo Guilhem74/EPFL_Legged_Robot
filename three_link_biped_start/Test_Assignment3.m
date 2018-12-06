5dbe6cc93e51da2736f4bfdabd2f5c80205b0ec3
Guilhem AZZANO <g.azzano74@gmail.com>
2018-12-06 08:54:23 +0100

    Robot walking basically

--- three_link_biped_start/Test_Assignment3.m
+++ three_link_biped_start/Test_Assignment3.m
@@ -1,5 +1,5 @@
 close all;
 clear all;
-sln=solve_eqns([0;0; 0],[0.2;0;0],15);
+sln=solve_eqns([0;0; 0],[0.1;0;0],50);
 animate(sln);
 analyze(sln);
--- three_link_biped_start/control/control.m
+++ three_link_biped_start/control/control.m
@@ -17,17 +17,24 @@ P = inv(M)*B;
 Q = inv(M)*(C*dq - G);
 
 %u1=1/P(3)*(-Kp1*(q3)-Kd1*dq3+Q(3))*3;
-u1=Kp1*(q3-pi/8)+Kd1*dq3;
 
+if step_number>1
+    u1=Kp1*(q3-pi/6)+Kd1*dq3;
+else
+    u1=Kp1*(q3)+Kd1*dq3;
+end;
 if(u1>30)
     u1=30;
 elseif(u1<-30)
     u1=-30;
 end;
 u(1)=u1;
- Kp2=-50;
+
+ step_length=pi/8;
+ Kp2=-500;
  Kd2=-5;
- u2=Kp2*(q2+atan((q1)*1000)/(pi/2)*pi/8)+Kd2*(dq2-dq1*(1/(1+q1*q1))/(pi/2)*pi/5);
+ x_atan=q1*10;
+ u2=Kp2*(q2+atan((x_atan))/(pi/2)*step_length)+Kd2*(dq2+dq1*(1/(1+x_atan*x_atan))/(pi/2)*step_length);
 if(u2>30)
     u2=30;
 elseif(u2<-30)
