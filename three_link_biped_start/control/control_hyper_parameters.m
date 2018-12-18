7ca32cf9e8bcecab14c7ee98317fc96e621d56ea
Guilhem AZZANO <g.azzano74@gmail.com>
2018-12-18 21:35:30 +0100

    Remove useless files

--- three_link_biped_start/control/control_hyper_parameters.m
+++ three_link_biped_start/control/control_hyper_parameters.m
@@ -1,7 +1,7 @@
 % You can set any hyper parameters of the control function here; you may or
 % may not want to use the step_number as the input of the function. 
 function [speed_target, tanh_c, Kp1_2] = control_hyper_parameters()
-    speed_target=1.1;
+    speed_target=1.5;
     if speed_target==0.7
         tanh_c=40;
         Kp1_2=60;
