378a089b63a83df83a046749a02fa197675fb98a
Guilhem AZZANO <g.azzano74@gmail.com>
2018-12-19 09:17:44 +0100

    removing uglob

--- three_link_biped_start/solve_eqns/eqns.m
+++ three_link_biped_start/solve_eqns/eqns.m
@@ -13,7 +13,6 @@ M = eval_M(q);
 C = eval_C(q, dq);
 G = eval_G(q);
 B = eval_B();
-global u_var
 u = control(t, q, dq, q0, dq0, step_number); % for the moment we set the control outputs to zero
 u_ext = perturbation(q, step_number); % perturbation torque due to the external force at hip
 
