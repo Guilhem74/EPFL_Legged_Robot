39db3000223e33251f5f9745615434721239f777
Guilhem AZZANO <g.azzano74@gmail.com>
2018-12-19 22:21:23 +0100

    Cleaning control.m

--- three_link_biped_start/control/control.m
+++ three_link_biped_start/control/control.m
@@ -1,6 +1,6 @@
 function u = control(t, q, dq, q0, dq0, step_number)
-%WARNING : Only q and dq are set in the analyse file, if you must use that
-%function please change the call in the analyze.m file
+%WARNING : Only q and dq are set in the analyse file, if you must use more
+%parameters please change the call in the analyze.m file
 u = zeros(2, 1);
 
 [step_angle,Kp1,Kd1,Kp2,Kd2,tanh_c, Kp1_2,speed] = control_hyper_parameters();
@@ -21,13 +21,8 @@ Q = M\(C*dq - G);
 
 [~, ~, dq_h, ~] = kin_hip(q,dq);
 
-%u1_1 = Kp1*(q3 - head_angle*(q2+step_angle)/(2*step_angle)) + Kd1*(dq3-head_angle*dq2/(2*step_angle));
-%u1_1 = Kp1*(q3) + Kd1*dq3;
-%u1_1 = 1/(P(3)-P(1))*(-Kp1*(q3) - Kd1*(dq3-dq1) + (Q(3)-Q(1)));
-
-u1_1 = 1/(P(3)-P(1))*(-Kp1*(q3-q1) - Kd1*(dq3-dq1) + (Q(3)-Q(1)));
-u1_2 = Kp1_2*(dq_h-speed);
-%u1_3 = Kp1_3*exp(-q2*1e1)/exp(step_angle*1e1);
+u1_1 = 1/(P(3)-P(1))*(-Kp1*(q3-q1) - Kd1*(dq3-dq1) + (Q(3)-Q(1))); %Following q1
+u1_2 = Kp1_2*(dq_h-speed); % regulating speed
 
 u1 = u1_1 + u1_2;
 
